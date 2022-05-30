7a3aa98127f00851d6dadf04ca5277ef18c8a8d6
SEANYB4 <bainsean@outlook.com>
2022-05-30 19:06:50 +0100

    Setup react app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes, 
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    
+    <Router>
+          <div className="head">
+          <h1>Hello English Teachers</h1>
+         </div>
+
+         <div>
+            <nav className='nav'>
+              <ul className='navList'>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/lessons">Lessons</Link>
+                </li>
+                <li>
+                  <Link to="/forum">Forum</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* <Routes>
+              <Route path="/about">
+                <About />
+              </Route>
+
+              <Route path='/lessons'>
+                <Lessons />
+              </Route>
+
+              <Route path='/'>
+                <Home />
+              </Route>
+
+              <Route path='/forum'>
+                <Forum />
+              </Route>
+
+            </Routes> */}
+         </div>
+    </Router>
   );
+}
+
+
+
+function About() {
+  return <h2>Home</h2>
+}
+
+function Lessons() {
+  return <h2>Lessons</h2>
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Forum() {
+  return <h2>Forum</h2>
 }
 
 export default App;
